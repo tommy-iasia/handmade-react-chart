11b450bd3c3f0af5474cdc270957d96eff5ec363
tommylee <tommylee@iasia.com.hk>
2022-01-07 18:30:09 +0800

    donut label

--- src/donuts/labels/DrawLabel.tsx
+++ src/donuts/labels/DrawLabel.tsx
@@ -1,24 +1,46 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useContext } from "react";
+import { DonutChartContext } from "../DonutChartContext";
 import { getPoint } from "../DrawSlice";
 import "./DrawLabel.css";
 
 export function DrawLabel({
   centerX,
   centerY,
-  radius,
+  radius: labelRadius,
   angle,
   children,
 }: Props) {
-  const point = getPoint(centerX, centerY, radius, angle);
+  const point = getPoint(centerX, centerY, labelRadius, angle);
+
+  const side = angle <= 90 || angle >= 270 ? "right" : "left";
+
+  const { innerRadius, outerRadius } = useContext(DonutChartContext);
+  const lineWidth = labelRadius - (outerRadius * 0.7 + innerRadius * 0.3);
 
   return (
     <div
-      className={`handmadeReactChart-donuts-labels-DrawLabel ${
-        angle <= 90 || angle >= 270 ? "right" : "left"
-      }`}
+      className={`handmadeReactChart-donuts-labels-DrawLabel ${side}`}
       style={{ left: `${point.x}px`, top: `${point.y}px` }}
     >
-      {children}
+      {side === "right" && (
+        <div className="point">
+          <div
+            className="line"
+            style={{ width: lineWidth, transform: `rotate(${angle + 180}deg)` }}
+          ></div>
+        </div>
+      )}
+
+      <div className="content">{children}</div>
+
+      {side === "left" && (
+        <div className="point">
+          <div
+            className="line"
+            style={{ width: lineWidth, transform: `rotate(${angle + 180}deg)` }}
+          ></div>
+        </div>
+      )}
     </div>
   );
 }
